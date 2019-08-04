8fd783309c9f4fc9709194f9de6198aad0ea7b1c
shans96 <shanswanlow96@gmail.com>
2019-08-04 17:37:31 +0200

    Add Firebase utility class

---
+++ app/src/main/java/com/shanswanlow/travelmantics/FirebaseUtil.java
@@ -0,0 +1,29 @@
+package com.shanswanlow.travelmantics;
+
+import android.provider.ContactsContract;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class FirebaseUtil
+{
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static ArrayList<TravelDeal> mDeals;
+
+    private FirebaseUtil(){};
+
+    public static void openFirebaseReference(String pathReference)
+    {
+        if (firebaseUtil == null)
+        {
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mDeals = new ArrayList<>();
+        }
+        mDatabaseReference = mFirebaseDatabase.getReference().child(pathReference);
+    }
+}
