a0b7f2abbaf8da763865b9b971259e0cac729557
shans96 <shanswanlow96@gmail.com>
2019-08-04 19:33:49 +0200

    Refactor ListActivity to use DealAdapter

--- app/src/main/java/com/shanswanlow/travelmantics/ListActivity.java
+++ app/src/main/java/com/shanswanlow/travelmantics/ListActivity.java
@@ -1,56 +1,47 @@
 package com.shanswanlow.travelmantics;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListActivity extends AppCompatActivity
 {
-    ArrayList<TravelDeal> deals;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildEventListener;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        FirebaseUtil.openFirebaseReference("traveldeals");
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        mChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
-            {
-                TextView travelDeals = findViewById(R.id.travelDeals);
-                TravelDeal travelDeal = dataSnapshot.getValue(TravelDeal.class);
-                travelDeals.setText(travelDeals.getText() + "\n" + travelDeal.getTitle());
-            }
+        RecyclerView dealsRecyclerView = findViewById(R.id.recyclerDeals);
+        final DealAdapter dealAdapter = new DealAdapter();
+        dealsRecyclerView.setAdapter(dealAdapter);
+        LinearLayoutManager dealsLayoutManager =
+                new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        dealsRecyclerView.setLayoutManager(dealsLayoutManager);
+    }
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.list_activity_menu, menu);
+        return true;
+    }
 
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {}
-        };
-        mDatabaseReference.addChildEventListener(mChildEventListener);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.insert_deal_menu)
+        {
+            Intent createDealIntent = new Intent(this, InsertActivity.class);
+            startActivity(createDealIntent);
+        }
+        return true;
     }
 }
